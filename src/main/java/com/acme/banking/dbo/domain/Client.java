89f8257cece34beec5787ecbf4f63a0d00023839
aazayka <aazayka@mail.ru>
2020-05-17 20:02:28 +0300

    * Guard clauses * Test for Client and SavingAccount
    Open questions:
    * Why hamcrest?

--- src/main/java/com/acme/banking/dbo/domain/Client.java
+++ src/main/java/com/acme/banking/dbo/domain/Client.java
@@ -4,13 +4,16 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.UUID;
 
+import static org.apache.commons.lang3.StringUtils.isEmpty;
+
 public class Client {
     private UUID id;
     private String name;
     private Collection<UUID> accountIds = new ArrayList<>(); //TODO
 
     public Client(UUID id, String name) {
-        if (id == null) throw new IllegalArgumentException();
+        if (id == null) throw new IllegalArgumentException("ID shouldn't be null");
+        if (isEmpty(name)) throw new IllegalArgumentException("Name shouldn't be empty");
 
         this.id = id;
         this.name = name;
